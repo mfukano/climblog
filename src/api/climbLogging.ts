35eabd74da8cd173064b51e16f49ab131d0f44d1
Mat Fukano <fukano.mat@gmail.com>
2024-05-03 01:43:09 -0700

    Cleanup

--- src/api/climbLogging.ts
+++
@@ -1,6 +0,0 @@
-
-
-// here, I want to collect state through an API interface
-// we'll need to pass some variables to the parameters of this function
-
-// See https://www.notion.so/matfu/Design-Doc-6a318bcb219a49ee896215c4d0db7b38?pvs=4#84960b30263b45c8a66742b840cffaa5
