101c2aa49e8d7d05ecfce736e109a1ca6d5fed42
Mat Fukano <fukano.mat@gmail.com>
2024-05-03 01:53:02 -0700

    eslint and add Active Session to stackNav

--- src/nav/NavStack.tsx
+++ src/nav/NavStack.tsx
@@ -1,16 +1,17 @@
-import React from "react"
-import { SafeAreaView, View, Text, Button } from "react-native"
-import { createStackNavigator } from "@react-navigation/stack"
-import { LoggingScreen, SessionScreen } from "../views"
+import React from "react";
+// import { SafeAreaView, View, Text, Button } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { ActiveSession, LoggingScreen, SessionScreen } from "../views";
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 export default function NavStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Overview" component={SessionScreen} />
-      <Stack.Screen name="Log Climb" component={LoggingScreen} />
-    </Stack.Navigator>
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name="Sessions" component={SessionScreen} />
+			<Stack.Screen name="Log Climb" component={LoggingScreen} />
+			<Stack.Screen name="Active Session" component={ActiveSession} /> 
+		</Stack.Navigator>
 
-  )
+	);
 }
--- src/nav/TabStack.tsx
+++ src/nav/TabStack.tsx
@@ -1,19 +1,27 @@
-import * as React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { HomeScreen, ProfileScreen, LoggingScreen } from '../views'
-import { NavigationContainer } from '@react-navigation/native';
-import NavStack from './NavStack';
+import * as React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { 
+	HomeScreen, 
+	ProfileScreen, 
+	// LoggingScreen 
+} from "../views";
+import { NavigationContainer } from "@react-navigation/native";
+import NavStack from "./NavStack";
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 export default function TabStack() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-        <Tab.Screen name="Session" component={NavStack} options={{ headerShown: false }} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  )
+	return (
+		<NavigationContainer>
+			<Tab.Navigator>
+				<Tab.Screen name="Home" component={HomeScreen} />
+				<Tab.Screen name="Profile" component={ProfileScreen} />
+				<Tab.Screen
+					name="Session"
+					component={NavStack}
+					options={{ headerShown: false }}
+				/>
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
 }
