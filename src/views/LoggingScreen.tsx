08b6913aefcd5cd436c4743b623012f64ae7ee9c
Mat Fukano <fukano.mat@gmail.com>
2024-05-03 01:53:31 -0700

    eslint

--- src/views/LoggingScreen.tsx
+++ src/views/LoggingScreen.tsx
@@ -1,73 +1,84 @@
-import React, {useState} from 'react'
-import { Button, StyleSheet, ScrollView, View, Text, TextInput } from 'react-native'
-import { ColorPicker, ClimbingDisciplinePicker, GradePicker, ProgressPicker, TerrainPicker, ProblematicHoldsPicker } from '../components/logging-selectors'
-import { Divider } from '../components/basic-components'
-import { climbsStore } from '../store/climbStore'
+import React, {useState} from "react";
+import { 
+	Button, 
+	StyleSheet, 
+	ScrollView, 
+	View, 
+	Text, 
+	// TextInput 
+} from "react-native";
+import { ColorPicker, ClimbingDisciplinePicker, GradePicker, ProgressPicker, TerrainPicker, ProblematicHoldsPicker } from "../components/logging-selectors";
+import { Divider } from "../components/basic-components";
+import { climbsStore } from "../store/climbStore";
 
+// TODO: I need to set up StackProps before react navigation can pass through the linter
+// eslint-disable-next-line react/prop-types
 const LoggingScreen = ({ navigation }) => {
-  const [color, setColor] = useState("Red")
-  const [climbingDiscipline, setClimbingDiscipline] = useState("Boulder")
-  const [grade, setGrade] = useState("V0")
-  const [terrain, setTerrain] = useState([])
-  const [problemHolds, setProblemHolds] = useState([])
-  const [progress, setProgress] = useState("Projecting")
+	const [color, setColor] = useState("Red");
+	const [climbingDiscipline, setClimbingDiscipline] = useState("Boulder");
+	const [grade, setGrade] = useState("V0");
+	const [terrain, setTerrain] = useState([]);
+	const [problemHolds, setProblemHolds] = useState([]);
+	const [progress, setProgress] = useState("Projecting");
 
-  const handleSubmit = () => {
-    climbsStore.addClimb({
-      color: color,
-      discipline: climbingDiscipline,
-      grade: grade,
-      terrain: terrain,
-      problemHolds: problemHolds,
-      progress: progress
-    })
-    navigation.goBack()
-  } 
+	const handleSubmit = () => {
+		climbsStore.addClimb({
+			color: color,
+			discipline: climbingDiscipline,
+			grade: grade,
+			terrain: terrain,
+			problemHolds: problemHolds,
+			progress: progress
+		});
 
-  return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Divider text={"Discipline"} />
-      <ClimbingDisciplinePicker climbingDiscipline={climbingDiscipline} setClimbingDiscipline={setClimbingDiscipline}/>
-      <Divider text={"Hold Or Tape Color"} />
-      <ColorPicker color={color} setColor={setColor} />
-      <Divider text={"Grade"} />
-      <GradePicker climbingDiscipline={climbingDiscipline} selectedGrade={grade} setSelectedGrade={setGrade} />
-      <Divider text={"Terrain"} />
-      <TerrainPicker terrain={terrain} setTerrain={setTerrain} />
-      <Divider text={"Problematic Holds"} />
-      <ProblematicHoldsPicker problemHolds={problemHolds} setProblemHolds={setProblemHolds} />
-      <Divider text={"Progress"} />
-      <ProgressPicker progress={progress} setProgress={setProgress} />
-      <Button title={"Submit Climb"} color={"blue"} onPress={handleSubmit}/>
+		// eslint-disable-next-line react/prop-types
+		navigation.goBack();
+	}; 
 
-      <Divider text={"Page State"} />
-      <View style={styles.container}>
-        <Text>{climbingDiscipline}</Text>
-        <Text>{grade}</Text>
-        <Text>{terrain.join(',')}</Text>
-        <Text>{problemHolds.join(',')}</Text>
-        <Text>{progress}</Text>
-      </View>
+	return (
+		<ScrollView contentContainerStyle={styles.container}>
+			<Divider text={"Discipline"} />
+			<ClimbingDisciplinePicker climbingDiscipline={climbingDiscipline} setClimbingDiscipline={setClimbingDiscipline}/>
+			<Divider text={"Hold Or Tape Color"} />
+			<ColorPicker color={color} setColor={setColor} />
+			<Divider text={"Grade"} />
+			<GradePicker climbingDiscipline={climbingDiscipline} selectedGrade={grade} setSelectedGrade={setGrade} />
+			<Divider text={"Terrain"} />
+			<TerrainPicker terrain={terrain} setTerrain={setTerrain} />
+			<Divider text={"Problematic Holds"} />
+			<ProblematicHoldsPicker problemHolds={problemHolds} setProblemHolds={setProblemHolds} />
+			<Divider text={"Progress"} />
+			<ProgressPicker progress={progress} setProgress={setProgress} />
+			<Button title={"Submit Climb"} color={"blue"} onPress={handleSubmit}/>
 
-    </ScrollView>
-  )
-}
+			<Divider text={"Page State"} />
+			<View style={styles.container}>
+				<Text>{climbingDiscipline}</Text>
+				<Text>{grade}</Text>
+				<Text>{terrain.join(",")}</Text>
+				<Text>{problemHolds.join(",")}</Text>
+				<Text>{progress}</Text>
+			</View>
+
+		</ScrollView>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    margin: 20,
-    marginBottom: 50,
-  },
+	container: {
+		alignItems: "center",
+		margin: 20,
+		marginBottom: 50,
+	},
 
-  textinput: {
-    backgroundColor: 'white',
-    borderColor: "black",
-    borderStyle: "solid",
-    borderWidth: 1,
-    fontSize: 20,
-    width: '50%',
-  }
-})
+	textinput: {
+		backgroundColor: "white",
+		borderColor: "black",
+		borderStyle: "solid",
+		borderWidth: 1,
+		fontSize: 20,
+		width: "50%",
+	}
+});
 
-export default LoggingScreen
+export default LoggingScreen;
--- src/views/ProfileScreen.tsx
+++ src/views/ProfileScreen.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from "react";
+import { Text } from "react-native";
 
 const ProfileScreen = () => {
-  return (
-    <Text>Hello Profile Screen!</Text>
-  )
-}
+	return (
+		<Text>Hello Profile Screen!</Text>
+	);
+};
 
-export default ProfileScreen
+export default ProfileScreen;
