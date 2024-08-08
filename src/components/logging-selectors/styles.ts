1481edd64f0c944523ea9c9a978d60412fe81478
Mat Fukano <fukano.mat@gmail.com>
2024-08-08 02:13:07 -0700

    Rename picker styles for /logging screne, add formStyles for /manage and /active

--- src/components/logging-selectors/styles.ts
+++ src/components/logging-selectors/styles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
+const pickerStyles = StyleSheet.create({
 	container: {
 		alignItems: "center",
 		display: "flex",
@@ -29,4 +29,43 @@ const styles = StyleSheet.create({
 	},
 });
 
-export default styles;
+const formStyles = StyleSheet.create({
+	container: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		marginBottom: 50,
+		width: "100%",
+	},
+	climbListContainer: {
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		width: "90%",
+		marginTop: -10,
+
+	},
+	climbItemContainer: {
+		backgroundColor: "white",
+		padding: 10,
+		borderRadius: 3,
+		width: "100%",
+		marginTop: 10,
+	},
+	climbItemHeader: {
+		fontWeight: "bold",
+	},
+	textinput: {
+		backgroundColor: "white",
+		borderColor: "black",
+		borderStyle: "solid",
+		borderWidth: 1,
+		fontSize: 20,
+		width: "30%",
+	},
+});
+
+export {
+	pickerStyles,
+	formStyles,
+};
