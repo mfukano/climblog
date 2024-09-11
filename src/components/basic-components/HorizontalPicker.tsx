35a85c426332de8941e03cc0dfedcaf0a141750e
Mat Fukano <fukano.mat@gmail.com>
2024-09-10 22:12:49 -0700

    Horizontal picker seems to be working as expected now, but lots of customization to do to make it work for climb logging's toggle menu

--- src/components/basic-components/HorizontalPicker.tsx
+++ src/components/basic-components/HorizontalPicker.tsx
@@ -1,21 +1,89 @@
 import React from "react";
-import Picker from "react-native-picker-select";
+// import Picker from "react-native-picker-select";
+import { Animated, StyleSheet, Text } from "react-native";
+import { HorizontalScrollPicker } from "react-native-horizontal-scroll-picker";
 import { View } from "react-native";
 
 type PickerProps = {
-	callback: () => void,
+	stateVariable: string,
+	stateSetFn: (stateVariable) => void,
 }
 
-export default function HorizontalPicker({
-	callback
+const ITEM_WIDTH = 40;
+const ITEMS = Array.from(Array(15).keys()).map((val, idx) => {
+	return {
+		label: val,
+		value: idx,
+	};
+});
+
+export default function HorizantalPicker({
+	stateVariable,
+	stateSetFn
 }: PickerProps) {
-	
+	// const [scrollX, setScrollX] = React.useState(new Animated.Value(20));
+	const [selected, setSelected] = React.useState(3);
+
+	const updateState = (newValue) => {
+		console.log(`newValue: ${newValue}`);
+		setSelected(newValue);
+		stateSetFn(newValue);
+	};
+
+	const opacityAnimation = React.useRef(new Animated.Value(0)).current;
+	const opacityStyle = { opacity: opacityAnimation };
+    
+	const animateOpacity = () => {
+		Animated.timing(opacityAnimation, {
+			toValue: 0,
+			duration: 0,
+			useNativeDriver: true
+		}).start(() => {
+			Animated.timing(opacityAnimation, {
+				toValue: 1,
+				duration: 0,
+				useNativeDriver: true,
+			}).start();
+		});
+	};
+
 	return (
-		<View>
-
-			<Picker>
-
-			</Picker>
+		<View id={"h-picker-test"} style={{
+			/*
+			 * width: "100%",
+			 * height: 200,
+			 */
+		}}>
+			<HorizontalScrollPicker
+				items={ITEMS}
+				onSelect={(newValue) => updateState(newValue)}
+				initialIdx={selected}
+				containerStyle={styles.container}
+				selectorStyle={styles.selectorStyle}
+				// scrollX={scrollX}
+				/*
+				 * textStyle={styles.textStyle}
+				 * selectedTextStyle={styles.selectedTextStyle}
+				 * itemStyle={styles.itemStyle}
+				 */
+			/>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		width: "100%",
+		height: 100
+	},
+	itemStyle: {
+		// padding: 20,
+	},
+	textStyle: {
+	},
+	selectedTextStyle: {
+	},
+	selectorStyle: {
+		// backgroundColor: "cyan",
+	}
+});
