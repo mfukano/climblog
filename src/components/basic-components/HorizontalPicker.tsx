90c20008b3f60c64d020db5b18fdb13b0821da2f
Mat Fukano <fukano.mat@gmail.com>
2024-10-01 05:55:39 -0700

    type cleanup

--- src/components/basic-components/HorizontalPicker.tsx
+++ src/components/basic-components/HorizontalPicker.tsx
@@ -2,63 +2,51 @@ import React from "react";
 // import Picker from "react-native-picker-select";
 import { StyleSheet } from "react-native";
 import { HorizontalScrollPicker } from "react-native-horizontal-scroll-picker";
-import { View } from "react-native";
 
 type PickerProps = {
-	stateVariable: string,
-	stateSetFn: (stateVariable) => void,
-	items?: string[]
-	selectorStyle?: object
-}
+	stateSetFn: React.Dispatch<React.SetStateAction<string>>;
+	items?: string[];
+};
 
 /**
- * 
+ *
  * @param stateVariable - I was passing this in for some reason but I think I might not need to use it
  * @param stateSetFn - Passed setState() function to manage state for page using the picker
  * @param items - The string arrays to iterate over for picker items
- * @returns 
+ * @returns
  */
-export default function HorizantalPicker({
-	stateVariable,
-	stateSetFn,
-	items,
-	selectorStyle,
-}: PickerProps) {
+export default function HorizantalPicker({ stateSetFn, items }: PickerProps) {
 	const [selected, setSelected] = React.useState(0);
 
-	const itemsFallback = items || Array.from(Array(15).keys());
+	const itemsFallback =
+		items || Array.from(Array(15).keys()).map((a) => a.toString());
 
-	const ITEMS = itemsFallback.map((val, idx) => {
+	const ITEMS = itemsFallback.map((val: string, idx: number) => {
 		return {
 			label: val,
 			value: idx,
 		};
 	});
 
-	const updateState = (newValue) => {
-		console.log(`
-			newValue: ${newValue}
-			items[newValue]: ${items[newValue]}
-		`);
+	const updateState = (newValue: number) => {
 		setSelected(newValue);
 		stateSetFn(items[newValue]);
 	};
 
 	return (
-		<View>
-			<HorizontalScrollPicker
-				items={ITEMS}
-				onSelect={(newValue) => updateState(newValue)}
-				initialIdx={selected}
-				containerStyle={styles.container}
-			/>
-		</View>
+		<HorizontalScrollPicker
+			items={ITEMS}
+			onSelect={(newValue: number) => updateState(newValue)}
+			initialIdx={selected}
+			containerStyle={styles.container}
+		/>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		width: "100%",
-		height: 100
+		height: 100,
 	},
 });
+
